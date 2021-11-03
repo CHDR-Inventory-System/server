2af254535a85424e1129cd71eb3fd4104b8c42a2
ctcuff <dev.ctcuff@gmail.com>
2021-11-02 21:58:46 -0400

    Implemented SQL querying logic

--- src/app.py
+++ src/app.py
@@ -1,10 +1,30 @@
+import mysql.connector
 import sys
+import os
+from dotenv import load_dotenv
 from flask import Flask, jsonify, render_template
 from random import randint
+
+load_dotenv()
+
+try:
+    connection = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USERNAME'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_DATABASE'),
+    )
+except Exception as err:
+    print(err)
+    exit(1)
 
 app = Flask(
     __name__, template_folder='../build', static_folder='../build', static_url_path=''
 )
+
+
+def create_error_response(message, status_code):
+    return jsonify({'message': message}), status_code
 
 
 @app.route('/')
@@ -12,14 +32,34 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/message')
-def message():
-    return jsonify({'message': 'Hello, World!'})
+@app.route('/api/items')
+def get_item():
+    try:
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM example')
+        items = []
 
+        for row in cursor.fetchall():
+            item_id, name, desc, date, moveable, quantity = row
 
-@app.route('/random')
-def random_number():
-    return jsonify({'number': randint(0, 100)})
+            items.append(
+                {
+                    'id': item_id,
+                    'name': name,
+                    'description': desc,
+                    'date': date,
+                    'moveable': bool(moveable),
+                    'quantity': quantity,
+                }
+            )
+
+        cursor.close()
+
+        return jsonify(items)
+
+    except Exception as err:
+        print(err)
+        return create_error_response('Item not found', 404)
 
 
 if __name__ == '__main__':
