515d990e22808f9eafb73e4c1ebbe340c205ecc9
ctcuff <dev.ctcuff@gmail.com>
2022-01-14 18:02:57 -0500

    Add additional routes to query reservations
    - reservations can be searched by status, item id, user id, and
      reservation id

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import sys
-import routes.inventory
 import logging
+from routes.reservation import reservation_blueprint
 from flask import Flask, render_template
 from flask_cors import CORS
 
@@ -10,14 +10,11 @@ app = Flask(
 
 CORS(app)
 
-# This makes sure that log messages get written to /logs/gunicorn_error.log
 gunicorn_logger = logging.getLogger("gunicorn.error")
 app.logger.handlers = gunicorn_logger.handlers
 app.logger.setLevel(gunicorn_logger.level)
 
-app.register_blueprint(
-    routes.inventory.inventory_blueprint, url_prefix="/api/inventory"
-)
+app.register_blueprint(reservation_blueprint, url_prefix="/api/reservations")
 
 
 @app.route("/")
--- src/routes/reservation.py
+++ src/routes/reservation.py
@@ -5,38 +5,95 @@ from util.response import create_error_response, convert_javascript_date
 
 reservation_blueprint = Blueprint("reservation", __name__)
 
+VALID_RESERVATION_STATUSES = {
+    "pending",
+    "denied",
+    "approved",
+    "missed",
+    "checked out",
+    "late",
+    "returned",
+}
 
-@reservation_blueprint.route("/user/<int:user_id>", methods=["GET"])
+
 @Database.with_connection
-def get_user_reservations(user_id, **kwargs):
+def query_reservations(base_query: str, **kwargs):
+    """
+    A helper function that uses "base_query" to select reservations from
+    the reservation table and build a JSON structure that includes the
+    item, user, and admin who approves it
+    """
     cursor = kwargs["cursor"]
 
     try:
-        cursor.execute("SELECT * FROM reservation WHERE user = %s" % (user_id,))
+        cursor.execute(base_query)
         reservations = cursor.fetchall()
 
-        for row in reservations:
-            cursor.execute("SELECT * FROM item WHERE ID = %s", (row["item"],))
-            row["item"] = cursor.fetchone()
-            row["item"]["moveable"] = bool(row["item"]["moveable"])
-            row["item"]["available"] = bool(row["item"]["available"])
+        # For every reservation, we'll need to replace the item id,
+        # user id, and admin id with the actual values in the database
+        for reservation in reservations:
+            cursor.execute("SELECT * FROM item WHERE ID = %s" % (reservation["item"],))
 
-            if row["userAdminID"] is not None:
+            reservation["item"] = cursor.fetchone()
+            reservation["item"]["moveable"] = bool(reservation["item"]["moveable"])
+            reservation["item"]["available"] = bool(reservation["item"]["available"])
+
+            cursor.execute(
+                "SELECT email, ID, role FROM users WHERE ID = %s"
+                % (reservation["user"],)
+            )
+
+            reservation["user"] = cursor.fetchone()
+
+            if reservation["userAdminID"] is not None:
                 cursor.execute(
                     """
                     SELECT email, ID, role FROM users
                     WHERE ID = %s AND (role = 'Admin' OR role = 'Super')
-                    """,
-                    (row["userAdminID"],),
+                    """
+                    % (reservation["userAdminID"],),
                 )
-                row["admin"] = cursor.fetchone()
+                reservation["admin"] = cursor.fetchone()
             else:
-                row["admin"] = None
+                reservation["admin"] = None
 
         return jsonify(reservations)
     except mysql.connector.Error as err:
         current_app.logger.exception(str(err))
     return create_error_response("An unexpected error occurred", 500)
+
+
+@reservation_blueprint.route("/", methods=["GET"])
+@Database.with_connection
+def get_all_reservations(**kwargs):
+    status = request.args.get("status", default="", type=str)
+
+    if status:
+        return query_reservations(
+            'SELECT * FROM reservation WHERE status = "%s"' % (status,)
+        )
+
+    return query_reservations("SELECT * FROM reservation")
+
+
+@reservation_blueprint.route("/user/<int:user_id>", methods=["GET"])
+@Database.with_connection
+def get_reservations_by_user(user_id, **kwargs):
+    return query_reservations("SELECT * FROM reservation WHERE user = %s" % (user_id,))
+
+
+@reservation_blueprint.route("/item/<int:item_id>", methods=["GET"])
+@Database.with_connection
+def get_reservations_by_item(item_id, **kwargs):
+    return query_reservations("SELECT * FROM reservation WHERE item = %s" % (item_id,))
+
+
+@reservation_blueprint.route("/<int:reservation_id>", methods=["GET"])
+@Database.with_connection
+def get_reservations_by_id(reservation_id, **kwargs):
+    return query_reservations(
+        "SELECT * FROM reservation WHERE ID = %s" % (reservation_id,)
+    )
 
 
 @reservation_blueprint.route("/", methods=["POST"])
@@ -58,13 +115,13 @@ def create_reservation(**kwargs):
         return create_error_response(f"Parameter {err.args[0]} is required", 400)
 
     try:
-        cursor.execute("SELECT ID from item WHERE ID = %s", (reservation["item"],))
+        cursor.execute("SELECT ID from item WHERE ID = %s" % (reservation["item"],))
         result = cursor.fetchone()
 
         if result is None:
             return create_error_response("Invalid item ID", 400)
 
-        cursor.execute("SELECT ID from users WHERE ID = %s", (reservation["user"],))
+        cursor.execute("SELECT ID from users WHERE ID = %s" % (reservation["user"],))
         result = cursor.fetchone()
 
         if result is None:
@@ -97,6 +154,35 @@ def delete_reservation(reservation_id, **kwargs):
 
     try:
         cursor.execute("DELETE FROM reservation WHERE ID = %s" % (reservation_id,))
+        connection.commit()
+    except mysql.connector.Error as err:
+        current_app.logger.exception(str(err))
+        return create_error_response("An unexpected error occurred", 500)
+
+    return jsonify({"status": "Success"})
+
+
+@reservation_blueprint.route("/<int:reservation_id>/status", methods=["PUT"])
+@Database.with_connection
+def update_status(reservation_id, **kwargs):
+    cursor = kwargs["cursor"]
+    connection = kwargs["connection"]
+
+    post_data = request.get_json()
+
+    try:
+        status = post_data["status"]
+    except KeyError:
+        return create_error_response("A status is required", 400)
+
+    if status.lower() not in VALID_RESERVATION_STATUSES:
+        return create_error_response("Invalid reservation status", 400)
+
+    try:
+        cursor.execute(
+            "UPDATE reservation SET status = '%s' WHERE ID = %s"
+            % (status, reservation_id)
+        )
         connection.commit()
     except mysql.connector.Error as err:
         current_app.logger.exception(str(err))
