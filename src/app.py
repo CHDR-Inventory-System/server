d23f937ab54f88515a0306fb641ff9bd8a1b600a
jcherisma <jeffreycherisma@yahoo.com>
2022-02-01 20:51:54 -0500

    Ldap fixed after pull request comments

--- src/app.py
+++ src/app.py
@@ -1,8 +1,6 @@
 import sys
 from routes.users import users_blueprint
 import logging
-from routes.reservation import reservation_blueprint
-from routes.inventory import inventory_blueprint
 from flask import Flask, render_template
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -22,8 +20,6 @@ app.logger.handlers = gunicorn_logger.handlers
 app.logger.setLevel(gunicorn_logger.level)
 
 app.register_blueprint(users_blueprint, url_prefix="/api/users")
-app.register_blueprint(reservation_blueprint, url_prefix="/api/reservations")
-app.register_blueprint(inventory_blueprint, url_prefix="/api/inventory")
 
 
 @app.route("/")
--- src/routes/users.py
+++ src/routes/users.py
@@ -52,31 +52,31 @@ def register(**kwargs):
         # Takes incoming data as json
         incoming_data = request.get_json()
 
-        nid_ = incoming_data["nid"]
-        password_ = incoming_data["password"]
-        email_ = incoming_data["email"]
+        nid = incoming_data["nid"]
+        password = incoming_data["password"]
+        email = incoming_data["email"]
         # set role and verified automatically to user and Admin and Super admnin or 0 to verified
-        verified_ = 0
-        role_ = "User"
+        verified = 0
+        role = "User"
 
         # If variables were inserted then proceed
-        if nid_ and password_ and email_:
+        if nid and password and email:
 
             try:
-                ucf_creds = ldap_auth(nid_, password_)
+                ucf_creds = ldap_auth(nid, password)
 
                 first_name = ucf_creds["entries"][0]["attributes"]["givenName"]
                 last_name = ucf_creds["entries"][0]["attributes"]["sn"]
                 full_name = f"{first_name} {last_name}"
 
                 # sql query to check if Email exists already
-                cursor.execute("SELECT * FROM users WHERE email = %s", (email_,))
+                cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
                 exist_acc = cursor.fetchone()
 
                 # Check to see if account exist already or not
                 if exist_acc:
-                    return create_error_response("Account already exists!", 208)
-                elif not re.match(r"[^@]+@[^@]+\.[^@]+", email_):
+                    return create_error_response("Account already exists!", 409)
+                elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
                     return create_error_response("Invalid email address!", 409)
                 else:
                     sql_Query2 = """
@@ -84,10 +84,10 @@ def register(**kwargs):
                   VALUES(%s, %s, %s, %s, %s)
                 """
                     data = (
-                        nid_,
-                        email_,
-                        verified_,
-                        role_,
+                        nid,
+                        email,
+                        verified,
+                        role,
                         full_name,
                     )
                 cursor.execute(sql_Query2, data)
@@ -105,11 +105,11 @@ def register(**kwargs):
                 current_app.logger.exception(str(err))
                 return create_error_response(str(err), 503)
         else:
-            return create_error_response("Please enter the required fields!", 204)
+            return create_error_response("Please enter the required fields!", 409)
 
     except Exception as err:
         current_app.logger.exception(str(err))
-        return create_error_response("Error", 500)
+        return create_error_response("An unexpected error occurred", 500)
 
 
 @users_blueprint.route("/login", methods=["POST"])
@@ -121,16 +121,16 @@ def login(**kwargs):
         # Takes incoming data as json
         incoming_data = request.get_json()
 
-        nid_ = incoming_data["nid"]
-        password_ = incoming_data["password"]
+        nid = incoming_data["nid"]
+        password = incoming_data["password"]
 
         # If variables were inserted then proceed
-        if nid_ and password_:
+        if nid and password:
             try:
-                ucf_creds = ldap_auth(nid_, password_)
+                ucf_creds = ldap_auth(nid, password)
                 ucf_creds_nid = ucf_creds["entries"][0]["attributes"]["cn"]
 
-                if ucf_creds_nid != nid_:
+                if ucf_creds_nid != nid:
                     return create_error_response("Invalid credentials", 401)
                 # sql query to check if nid exists already
                 cursor.execute("SELECT * FROM users WHERE nid = %s", (ucf_creds_nid,))
@@ -138,7 +138,7 @@ def login(**kwargs):
 
                 if exist_acc:
                     my_token = create_access_token(
-                        identity={"id": exist_acc["ID"], "role": exist_acc["role"]}
+                        identity={"ID": exist_acc["ID"], "role": exist_acc["role"]}
                     )
                     return jsonify({"token": my_token})
 
@@ -153,10 +153,10 @@ def login(**kwargs):
                 return create_error_response(str(err), 503)
 
         else:
-            return create_error_response("Please enter all required fields!", 204)
+            return create_error_response("Please enter all required fields!", 409)
     except Exception as err:
         current_app.logger.exception(str(err))
-        return create_error_response("Error", 500)
+        return create_error_response("An unexpected error occurred", 500)
 
 
 @users_blueprint.route("/<int:id>", methods=["DELETE"])
@@ -165,10 +165,10 @@ def delete(id, **kwargs):
     cursor = kwargs["cursor"]
     connection = kwargs["connection"]
     try:
-        id_ = id
+        my_id = id
         # checks to see if user exist or not
         sqlQuery_1 = "SELECT * FROM users WHERE ID = %s"
-        cursor.execute(sqlQuery_1, (id_,))
+        cursor.execute(sqlQuery_1, (my_id,))
         exist_acc = cursor.fetchone()
 
         if not exist_acc:
@@ -178,8 +178,8 @@ def delete(id, **kwargs):
         sqlQuery_2 = "DELETE FROM users WHERE ID=%s"
 
         try:
-            cursor.execute("DELETE FROM reservation WHERE user = %s" % (id_,))
-            cursor.execute(sqlQuery_2 % (id_,))
+            cursor.execute("DELETE FROM reservation WHERE user = %s" % (my_id,))
+            cursor.execute(sqlQuery_2 % (my_id,))
 
             connection.commit()
         except Exception as err:
@@ -191,31 +191,43 @@ def delete(id, **kwargs):
     except Exception as err:
         current_app.logger.exception(str(err))
         connection.rollback()
-        return create_error_response("Error", 500)
+        return create_error_response("An unexpected error occurred", 500)
 
 
 @users_blueprint.route("/<int:id>", methods=["GET"])
 @Database.with_connection
 def get_user_byID(id, **kwargs):
     cursor = kwargs["cursor"]
+    try:
+        my_id = id
 
-    id_ = id
+        # sql query to check if user exists already
+        sqlQuery_1 = "SELECT * FROM users WHERE ID=%s"
+        cursor.execute(sqlQuery_1, (my_id,))
+        exist_acc = cursor.fetchone()
+        created = exist_acc["created"]
+        nid = exist_acc["nid"]
+        email = exist_acc["email"]
+        full_name = exist_acc["fullName"]
+        role = exist_acc["role"]
+        userID = exist_acc["ID"]
 
-    # sql query to check if user exists already
-    sqlQuery_1 = "SELECT * FROM users WHERE ID=%s"
-    cursor.execute(sqlQuery_1, (id_,))
-    exist_acc = cursor.fetchone()
-    created_ = exist_acc["created"]
-    nid_ = exist_acc["nid"]
-    email_ = exist_acc["email"]
-    full_name = exist_acc["fullName"]
+        if not exist_acc:
+            return create_error_response("User does not exist", 404)
 
-    if exist_acc:
         return jsonify(
-            {"Full Name": full_name, "nid": nid_, "email": email_, "created": created_}
+            {
+                "ID": userID,
+                "created": created,
+                "email": email,
+                "role": role,
+                "nid": nid,
+                "fullName": full_name,
+            }
         )
-    else:
-        return create_error_response("User does not exist", 404)
+    except mysql.connector.Error as err:
+        current_app.logger.exception(str(err))
+        return create_error_response("An unexpected error occurred", 500)
 
 
 @users_blueprint.route("/", methods=["GET"])
