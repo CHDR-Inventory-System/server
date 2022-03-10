07678269de6f6be803dcd82a2cd932bfc2e7ac5f
AshenSilverwolf <gilbert.gavin@outlook.com>
2022-03-10 16:09:34 -0500

    made create_reservation freely accessible

--- src/routes/reservation.py
+++ src/routes/reservation.py
@@ -175,7 +175,6 @@ def get_reservations_by_id(reservation_id, **kwargs):
 
 
 @reservation_blueprint.route("/", methods=["POST"])
-@require_roles(["admin", "super"])
 @Database.with_connection()
 def create_reservation(**kwargs):
     cursor = kwargs["cursor"]
